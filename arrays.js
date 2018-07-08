663217783d25a5c499330521dcc28b8a3ce8b359
Austin Mayden <austin.mayden@gmail.com>
2018-07-08 20:19:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,13 +9,3 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
-
-function addElementToEndOfArray(array, element) {
-  [...array, element]
-  return array
-}
-
-fuction destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
-}
