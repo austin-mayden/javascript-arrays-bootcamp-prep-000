181ac19f692e25f1b64d5b31fba15da910dcb518
Austin Mayden <austin.mayden@gmail.com>
2018-07-08 20:32:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.pop();
+  return array;
+}
