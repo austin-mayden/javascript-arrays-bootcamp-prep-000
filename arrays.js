b305a9059a40ddaeb81c675bde0455566819ad55
Austin Mayden <austin.mayden@gmail.com>
2018-07-08 20:10:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
-var chocolateBars = [snickers, hundred grand, kitkat, skittles];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToEndOfArray
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+}
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
